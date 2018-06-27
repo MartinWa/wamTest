43b5ee5184e0f6fb8f3222ed1fa175e9979967dd
Martin Wåger <martin.w@comaround.se>
2018-06-27 10:04:36 +0200

    hmm

--- wamTest/Settings.cs
+++ wamTest/Settings.cs
@@ -4,9 +4,9 @@ namespace wamTest
 {
     public class Settings : ISettings
     {
-        public IEnumerable<string> SupportedVideoTypes => new List<string>();
-        public string MediaServiceAccountName => "";
-        public string MediaServiceAccountKey => "";
-        public string AzureStorageConnectionString => "";
+        public IEnumerable<string> SupportedVideoTypes => new List<string> { ".mp4" }; //".3gp,.3g2,.3gp2,.asf,.avi,.dv,.m2ts,.m2v,.m4a,.mod,.mov,.mp4,.mpeg,.mpg,.mts,.ts,.wmv
+        public string MediaServiceAccountName => "devzeromedia";
+        public string MediaServiceAccountKey => "6dKcTKDLoeTGYM6K1d6hmlGRMCadKTfbPKjd02pgENI=";
+        public string AzureStorageConnectionString => "DefaultEndpointsProtocol=https;AccountName=devzerostorage;AccountKey=W14kuJudTLiyOCbTMALw4dsnx0YVjQKeYhx2ZQYAu56/0DZgvqz5AU/0D5RcNLbTrsIXbpZXeEjPrG1CqZ2n6g==";
     }
 }
